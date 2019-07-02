c11b3982e1f1241671d58cbe1cb09757cc53dcbb
Ryley Chapman <Ryley20@yahoo.com>
2019-07-01 21:00:45 -0500

    update products and tables in javascript

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -39,3 +39,55 @@ function showProducts(){
 		})
 }
 
+//Prompts the user to place an order, fulfills the order, and then calls the new order function
+function placeOrder(){
+	inquirer.prompt([{
+		name: 'selectId',
+		message: 'Please enter the ID of the product you wish to purchase',
+		validate: function(value){
+			var valid = value.match(/^[0-9]+$/)
+			if(valid){
+				return true
+			}
+				return 'Please enter a valid Product ID'
+		}
+	},{
+		name:'selectQuantity',
+		message: 'How many of this product would you like to order?',
+		validate: function(value){
+			var valid = value.match(/^[0-9]+$/)
+			if(valid){
+				return true
+			}
+				return 'Please enter a numerical value'
+		}
+	}]).then(function(answer){
+	connection.query('SELECT * FROM products WHERE id = ?', [answer.selectId], function(err, res){
+		if(answer.selectQuantity > res[0].StockQuantity){
+			console.log('Insufficient Quantity');
+			console.log('This order has been cancelled');
+			console.log('');
+			newOrder();
+		}
+		else{
+			amountOwed = res[0].Price * answer.selectQuantity;
+			currentDepartment = res[0].DepartmentName;
+			console.log('Thanks for your order');
+			console.log('You owe $' + amountOwed);
+			console.log('');
+			//update products table
+			connection.query('UPDATE products SET ? Where ?', [{
+				StockQuantity: res[0].StockQuantity - answer.selectQuantity
+			},{
+				id: answer.selectId
+			}], function(err, res){});
+			//update departments table
+			logSaleToDepartment();
+			newOrder();
+		}
+	})
+
+}, function(err, res){})
+};
+
+
