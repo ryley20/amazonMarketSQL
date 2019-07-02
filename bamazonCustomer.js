9786d9b373a6a34fd54870555195bd89725247a1
Ryley Chapman <Ryley20@yahoo.com>
2019-07-01 20:57:13 -0500

    display for items available in store with callback

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,26 +1,41 @@
-// var inquirer for customer usage
-// var inquirer = require("inquirer");
 
-// 
+var inquirer = require('inquirer');
+var mysql = require('mysql');
 
-var mysql = require("mysql");
+var amountOwed;
+var currentDepartment;
+var updateSales;
 
 var connection = mysql.createConnection({
-  host: "localhost",
-
-  // Your port; if not 3306
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "Souljaboy7",
-  database: "bamazon"
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'Souljaboy7',
+	database: 'Bamazon_db'
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-  createProduct();
+
+connection.connect(function(err){
+	if (err) throw err;
+	console.log('connected as id: ' + connection.threadId)
 });
+
+//FUNCTIONS
+//=============================================================================
+
+//Displays all items available in store and then calls the place order function
+function showProducts(){
+	connection.query('SELECT * FROM products', function(err, res){
+		if (err) throw err;
+		console.log('=================================================');
+		console.log('=================Items in Store==================');
+		console.log('=================================================');
+
+		for(i=0;i<res.length;i++){
+			console.log('Item ID:' + res[i].id + ' Product Name: ' + res[i].ProductName + ' Price: ' + '$' + res[i].Price + '(Quantity left: ' + res[i].StockQuantity + ')')
+		}
+		console.log('=================================================');
+		placeOrder();
+		})
+}
+
